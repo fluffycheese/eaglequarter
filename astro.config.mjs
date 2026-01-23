8dc539559ad02873f1abbe7e03cd97b85bdc9f49
stephenjackson <sjackson@highclerecastle.co.uk>
2026-01-23 12:15:40 +0000

    working astro build

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,5 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   site: "https://eaglequarter.co.uk",
   output: "static",
-  build: {
-    format: "directory"
-  }
+  trailingSlash: "always"
 });
