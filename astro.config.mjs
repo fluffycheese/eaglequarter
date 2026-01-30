ee6b8943ba31b1a243a8a28a05ec6312c09de1cf
stephenjackson <sjackson@highclerecastle.co.uk>
2026-01-30 15:59:18 +0000

    added footer icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://eaglequarter.co.uk",
+  integrations: [sitemap()],
   output: "static",
   trailingSlash: "always"
 });
+
